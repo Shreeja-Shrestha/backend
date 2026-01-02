3a83fdd47dee379459263bf189992c9587a90f4b
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-01-02 18:44:17 +0545

    made an API for login and signup page

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+const db = require("./db");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));
+
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.send("API Running");
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(3000, () => console.log("Server running on port 3000"));
+app.get("/test", (req, res) => {
+  res.json({ message: "Backend connected successfully" });
+});
+app.use((req, res, next) => {
+  console.log("REQUEST:", req.method, req.url);
+  next();
 });

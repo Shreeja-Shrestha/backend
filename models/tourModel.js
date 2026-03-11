6b173af4a284f2f53364de96526529660714afc9
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-03-11 21:43:43 +0545

    Updated the tourModel.js for the admin CRUD operation

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,9 +1,83 @@
 const db = require("../config/db");
 
-exports.getAllTours = (callback) => {
-  db.query("SELECT * FROM tours", callback);
+/* GET ALL TOURS */
+exports.getAllTours = function (callback) {
+
+  const sql = "SELECT * FROM tours ORDER BY created_at DESC";
+
+  db.query(sql, callback);
+
 };
 
-exports.getTourById = (id, callback) => {
-  db.query("SELECT * FROM tours WHERE id = ?", [id], callback);
+
+/* GET TOUR BY ID */
+exports.getTourById = function (id, callback) {
+
+  const sql = "SELECT * FROM tours WHERE id = ?";
+
+  db.query(sql, [id], callback);
+
+};
+
+
+/* CREATE TOUR */
+exports.createTour = function (tour, callback) {
+
+  const sql = `
+    INSERT INTO tours
+    (title, destination, price, duration, category, description, image)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  db.query(
+    sql,
+    [
+      tour.title,
+      tour.destination,
+      tour.price,
+      tour.duration,
+      tour.category,
+      tour.description,
+      tour.image
+    ],
+    callback
+  );
+
+};
+
+
+/* UPDATE TOUR */
+exports.updateTour = function (id, tour, callback) {
+
+  const sql = `
+    UPDATE tours
+    SET title=?, destination=?, price=?, duration=?, category=?, description=?, image=?
+    WHERE id=?
+  `;
+
+  db.query(
+    sql,
+    [
+      tour.title,
+      tour.destination,
+      tour.price,
+      tour.duration,
+      tour.category,
+      tour.description,
+      tour.image,
+      id
+    ],
+    callback
+  );
+
+};
+
+
+/* DELETE TOUR */
+exports.deleteTour = function (id, callback) {
+
+  const sql = "DELETE FROM tours WHERE id = ?";
+
+  db.query(sql, [id], callback);
+
 };

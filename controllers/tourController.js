3006c6adbf6fae7e0a73fbb111dcb4a9debb1045
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-03-11 21:44:55 +0545

    Updated the tourController.js for admin CRUD oeration

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,10 @@
 const tourModel = require("../models/tourModel");
 
-exports.getTours = (req, res) => {
 
-  tourModel.getAllTours((err, result) => {
+/* GET ALL TOURS */
+exports.getTours = function (req, res) {
+
+  tourModel.getAllTours(function (err, result) {
 
     if (err) {
       return res.status(500).json(err);
@@ -14,17 +16,81 @@ exports.getTours = (req, res) => {
 
 };
 
-exports.getTourById = (req, res) => {
+
+/* GET TOUR BY ID */
+exports.getTourById = function (req, res) {
 
   const id = req.params.id;
 
-  tourModel.getTourById(id, (err, result) => {
+  tourModel.getTourById(id, function (err, result) {
 
     if (err) {
       return res.status(500).json(err);
     }
 
     res.json(result[0]);
+
+  });
+
+};
+
+
+/* CREATE TOUR */
+exports.createTour = function (req, res) {
+
+  const tour = req.body;
+
+  tourModel.createTour(tour, function (err, result) {
+
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    res.json({
+      message: "Tour created successfully",
+      tourId: result.insertId
+    });
+
+  });
+
+};
+
+
+/* UPDATE TOUR */
+exports.updateTour = function (req, res) {
+
+  const id = req.params.id;
+  const tour = req.body;
+
+  tourModel.updateTour(id, tour, function (err, result) {
+
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    res.json({
+      message: "Tour updated successfully"
+    });
+
+  });
+
+};
+
+
+/* DELETE TOUR */
+exports.deleteTour = function (req, res) {
+
+  const id = req.params.id;
+
+  tourModel.deleteTour(id, function (err, result) {
+
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    res.json({
+      message: "Tour deleted successfully"
+    });
 
   });
 

f24c9e8f64847ed168b77ab8c981772aa07e4417
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-03-11 21:46:39 +0545

    Updated the tourRoutes.js for the admin CRUD operation

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+
 const tourController = require("../controllers/tourController");
 
+
+/* READ ROUTES */
 router.get("/", tourController.getTours);
 router.get("/:id", tourController.getTourById);
+
+
+/* ADMIN CRUD ROUTES */
+router.post("/", tourController.createTour);
+router.put("/:id", tourController.updateTour);
+router.delete("/:id", tourController.deleteTour);
+
 
 module.exports = router;

e71464b811c0897ae5d292891b2777019524aa53
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-03-12 23:29:55 +0545

    added the  path in server.js

--- server.js
+++ server.js
@@ -47,6 +47,10 @@ app.use("/api/favorites", favoriteRoutes);
 const tourRoutes = require("./routes/tourRoutes");
 
 app.use("/api/tours", tourRoutes);
+
+const notificationRoutes = require("./routes/notificationRoutes");
+app.use("/api/notifications", notificationRoutes);
+
 app.get("/", (req, res) => res.send("API Running"));
 app.get("/test", (req, res) => res.json({ message: "Backend connected successfully" }));
 

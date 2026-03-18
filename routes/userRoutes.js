cb048b87c104c2fc57dea474075a2b1334a425b6
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-03-18 22:07:45 +0545

    user profile update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 
-// GET USER PROFILE
+///  GET USER PROFILE
 router.get("/profile/:id", (req, res) => {
-
   const userId = req.params.id;
 
   const sql = `
@@ -14,7 +13,6 @@ router.get("/profile/:id", (req, res) => {
   `;
 
   db.query(sql, [userId], (err, result) => {
-
     if (err) {
       console.error(err);
       return res.status(500).json({ message: "Database error" });
@@ -25,9 +23,44 @@ router.get("/profile/:id", (req, res) => {
     }
 
     res.json(result[0]);
-
   });
+});
 
+
+/// UPDATE USER PROFILE
+router.put("/update", (req, res) => {
+  console.log("HEADERS:", req.headers);
+  console.log("BODY:", req.body);
+
+const { id, name, email } = req.body || {};
+  if (!id) {
+    return res.status(400).json({ error: "User ID required" });
+  }
+
+  const sql = `
+    UPDATE users 
+    SET name = ?, email = ?
+    WHERE id = ?
+  `;
+
+  db.query(sql, [name, email, id], (err, result) => {
+   if (err) {
+  console.error("DB ERROR:", err);
+  return res.status(500).json({ 
+    error: "Database error",
+    details: err.message   // 👈 ADD THIS LINE
+  });
+}
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "User not found or no change" });
+    }
+
+    res.json({
+      success: true,
+      message: "Profile updated successfully",
+    });
+  });
 });
 
 module.exports = router;

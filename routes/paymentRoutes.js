b2fe2ae6ef2da50a68a8bc459ecdc34c3df14211
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-03-25 23:48:04 +0545

    added the routes for receipt

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -5,5 +5,6 @@ const paymentController = require("../controllers/paymentController");
 router.post("/initiate-payment", paymentController.initiatePayment);
 router.get("/payment-success", paymentController.paymentSuccess);
 router.post("/verify-payment", paymentController.verifyPayment);
+router.get("/receipt/:booking_id", paymentController.getReceipt);
 
 module.exports = router;

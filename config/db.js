f5a36765f2b338c7f21bb50a808d27aafe762ad0
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-04-03 20:44:10 +0545

    added the port number also

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: '',
-  database: 'tourandtravel_db'
+  database: 'tourandtravel_db',
+  port: 3306
 });
 
 connection.connect((err) => {
